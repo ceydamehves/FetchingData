963db614ac4a4808c27711f9e41ed3872a985822
ceydamehves <cmehves.keklik@std.hku.edu.tr>
2019-08-16 19:20:50 +0300

    db edited.  dinamic date,month added

--- screens/Home.js
+++ screens/Home.js
@@ -12,22 +12,23 @@ export default class Home extends React.Component {
   constructor(props){
     super(props)
     this.state = {
-      words: []
+      tr: "hey",
+      en: "hey",
     }
     fetch("http://192.168.0.29:3000/words").then((response) => (response.json()))
     .then((response) => {
       console.log(response)
-    this.setState({words: response});
+      console.log(this.state)
+      date = new Date();
+    this.setState({tr: response["months"][date.getMonth()+1][date.getDate()]["tr"],en: response["months"][date.getMonth()+1][date.getDate()]["en"]});
     })
   }
   render () {
     return (
-      this.state.words.map((data,i) => (
-        <View key={i} style={styles.textContainer}>
-        <Text style={styles.text}>{data["tr"]} </Text>
+        <View style={styles.textContainer}>
+        <Text style={styles.text}>{this.state.en} : {this.state.tr} </Text>
       </View>
-      ))
-    );
+      );
   }
 }
 
